112f367d6b71aba9a54e952dcd176e3f052f851a
zapqqqwe <2496524403@qq.com>
2020-07-18 11:19:41 +0800

    主干提交

--- Hellogit/src/main/java/com/git/Helliogit.java
+++ Hellogit/src/main/java/com/git/Helliogit.java
@@ -5,6 +5,6 @@ public class Helliogit {
 
     public static void main(String[] args) {
         System.out.println("Hellogit!");
-        System.out.println("Hellogiqqt!");
+        System.out.println("Hello、；；giqqt!");
     }
 }
